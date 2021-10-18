2a402bff97b727aee7d25c1ffc75b81fb7d77e4c
Sergey Efremov <sergey.efremov@smartbics.com>
2021-10-18 19:51:26 +0300

    The first task done

---
+++ src/main/java/ru/gb/Homework1.java
@@ -0,0 +1,13 @@
+package ru.gb;
+
+public class Homework1 {
+    public static void main(String[] args) {
+        printThreeWords();
+    }
+
+    public static void printThreeWords() {
+        System.out.println("Orange");
+        System.out.println("Banana");
+        System.out.println("Apple");
+    }
+}
