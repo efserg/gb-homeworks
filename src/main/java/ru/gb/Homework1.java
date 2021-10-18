46654b5f02ad63d22c108b4876e35723296cad2a
Sergey Efremov <sergey.efremov@smartbics.com>
2021-10-18 20:02:05 +0300

    The second task done

--- src/main/java/ru/gb/Homework1.java
+++ src/main/java/ru/gb/Homework1.java
@@ -3,11 +3,16 @@ package ru.gb;
 public class Homework1 {
     public static void main(String[] args) {
         printThreeWords();
+        checkSunSign();
     }
 
     public static void printThreeWords() {
-        System.out.println("Orange");
-        System.out.println("Banana");
-        System.out.println("Apple");
+        System.out.println("Orange\nBanana\nApple");
+    }
+
+    public static void checkSunSign() {
+        int a = 13, b = 42;
+        int c = a + b;
+        System.out.println("Сумма " + (c >= 0 ? "положительная" : "отрицательная"));
     }
 }
